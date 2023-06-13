a268965a75a148ab6539b0d9657a0846fe682267
jaylawson <lawson.524@osu.edu>
2023-06-12 20:48:31 -0400

    Boiler Plate Files
    Basic files for assignment

--- Percolation.java
+++ Percolation.java
@@ -1,3 +1,21 @@
+/* *****************************************************************************
+ *  Compilation:  javac-algs4 Percolation.java
+ *  Execution:    java-algs4 Percolation input.txt
+ *  Dependencies: Percolation.java
+ *
+ *  This program takes the name of a file as a command-line argument.
+ *  From that file, it
+ *
+ *    - Reads the grid size n of the percolation system.
+ *    - Creates an n-by-n grid of sites (initially all blocked)
+ *    - Reads in a sequence of sites (row, col) to open.
+ *
+ *  After each site is opened, it draws full sites in light blue,
+ *  open sites (that aren't full) in white, and blocked sites in black,
+ *  with with site (0, 0) in the upper left-hand corner.
+ *
+ **************************************************************************** */
+
 public class Percolation {
 
     // creates n-by-n grid, with all sites initially blocked
@@ -32,6 +50,6 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
-        
+
     }
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,3 +1,21 @@
+/* *****************************************************************************
+ *  Compilation:  javac-algs4 PercolationStats.java
+ *  Execution:    java-algs4 PercolationStats input.txt
+ *  Dependencies: Percolation.java
+ *
+ *  This program takes the name of a file as a command-line argument.
+ *  From that file, it
+ *
+ *    - Reads the grid size n of the percolation system.
+ *    - Creates an n-by-n grid of sites (initially all blocked)
+ *    - Reads in a sequence of sites (row, col) to open.
+ *
+ *  After each site is opened, it draws full sites in light blue,
+ *  open sites (that aren't full) in white, and blocked sites in black,
+ *  with with site (0, 0) in the upper left-hand corner.
+ *
+ **************************************************************************** */
+
 public class PercolationStats {
 
     // perform independent trials on an n-by-n grid
